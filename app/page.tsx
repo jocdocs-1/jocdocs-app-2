f27e13647e6e013c1b608f471693323c1be101a5
Frank Albano <pureblue222@yahoo.com>
2026-05-02 15:21:57 -0400

    Fix logo image syntax

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,6 @@ export default function HomePage() {
         {/* LOGO */}
         <div className="mb-2 flex flex-col items-center">
           <Image
-            <img
   src="/jocdocs-logo-full-v2.png"
   alt="jocdocs"
   style={{ width: "150px", margin: "0 auto 18px auto", display: "block" }}
