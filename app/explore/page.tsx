a31cb848a92e69d6dd3fa4c575084d544d6febdc
Frank Albano <pureblue222@yahoo.com>
2026-06-28 10:42:01 -0400

    Improve Explore card school name spacing

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -191,7 +191,7 @@ const matchesSport =
             </div>
 
             {/* SCHOOL */}
-            <div className="relative z-20 -mt-5 pl-4 pr-0 md:-mt-6 md:pl-6">
+            <div className="relative z-20 -mt-5 pl-2 pr-0 md:-mt-6 md:pl-5">
               <div
                 title={card.school}
                 className="
